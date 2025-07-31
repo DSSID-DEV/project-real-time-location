96929d609db13f47d87203c1393d9c20663d93de
Demetrio Santana <demetrio.santana@liax.tech>
2025-07-31 20:22:56 -0300

    initialize the project

---
+++ backend-real-time-location/src/main/java/com/sistemas/dssid/real_time_location/RealTimeLocationApplication.java
@@ -0,0 +1,13 @@
+package com.sistemas.dssid.real_time_location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeLocationApplication.class, args);
+	}
+
+}
---
+++ backend-real-time-location/src/test/java/com/sistemas/dssid/real_time_location/RealTimeLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemas.dssid.real_time_location;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
